52a6e80037051eb77232aa1975da1b2ef2bb2433
Antonio Maroto <antoniomarotoblasco@gmail.com>
2023-04-20 12:24:05 +0200

    Añadida clase Sleep para el menú

---
+++ src/Sleep.java
@@ -0,0 +1,11 @@
+public class Sleep {
+
+    public static void pause(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            System.err.format("IOException: %s%n", e);
+        }
+    }
+
+}
